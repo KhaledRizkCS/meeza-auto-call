3a8ce2ac99c3db2fff0eff66429e6981f533d391
KRizk-Intouch <96003776+KRizk-Intouch@users.noreply.github.com>
2023-12-05 21:51:12 +0200

    feat: form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,8 +3,74 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
+  selectData = [
+    {
+      model: 'auto_call.Room',
+      pk: 1,
+      fields: {
+        name: 'CT ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 2,
+      fields: {
+        name: 'MRI ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 3,
+      fields: {
+        name: 'US ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 4,
+      fields: {
+        name: 'XRAY ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 5,
+      fields: {
+        name: 'ECG ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 6,
+      fields: {
+        name: 'EMG ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 7,
+      fields: {
+        name: 'EEG ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 8,
+      fields: {
+        name: 'DEXA ',
+      },
+    },
+    {
+      model: 'auto_call.Room',
+      pk: 9,
+      fields: {
+        name: 'MAMO ',
+      },
+    },
+  ];
 
+  constructor() {}
 }
